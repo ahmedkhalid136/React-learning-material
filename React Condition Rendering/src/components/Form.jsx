efbfa355d0bc903f6f321de2d5e49259475b023a
ahmedkhalid136 <ahmedkhalid.hta@gmail.com>
2021-01-10 15:39:32 +0500

    Initial Commit

---
+++ React Condition Rendering/src/components/Form.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Form(props) {
+  return (
+    <form className="form">
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+      {props.register ? <button type="submit">Login</button> : <div><input type="password" placeholder="Confirm Password" /><button type="submit">Register</button></div> }
+    </form>
+  );
+}
+
+export default Form;
